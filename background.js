630dfc68a5ce021d5fcd2fb32901ebbb7e4d22af
Roy Shmueli <2903801+2roy999@users.noreply.github.com>
2020-08-24 13:14:31 +0300

    Fix extension

--- background.js
+++ background.js
@@ -1,16 +1,19 @@
-chrome.runtime.onInstalled.addListener(() => {
+function setup() {
   fetch(chrome.runtime.getURL('configs/tau.json'))
     .then(response => response.json())
     .then(json => {
       const mappings = json.mappings
       Object.entries(mappings).forEach(([from, to]) => {
         chrome.webRequest.onBeforeRequest.addListener((details) => {
-          const url = new URL(details.url)
-          url.host = to
-          return { redirectUrl: url.toString() }
-        },
+            const url = new URL(details.url)
+            url.host = to
+            return { redirectUrl: url.toString() }
+          },
           { urls: [`*://${from}/*`] },
           ['blocking'])
       })
     })
-})
+}
+
+chrome.runtime.onInstalled.addListener(setup)
+chrome.runtime.onStartup.addListener(setup)
